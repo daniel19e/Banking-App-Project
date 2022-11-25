d24f4abba24d1160af54360a1a6f6d4e0e1fe8dd
Daniel <esc.daniel91@gmail.com>
2022-11-25 13:21:28 -0500

    added styling and form validation

--- application/__init__.py
+++ application/__init__.py
@@ -11,9 +11,9 @@ app.config['SECRET_KEY'] = 'vzj9ew5aeuqf5m19'
 
 db_connection = psycopg2.connect(
     host="localhost",
-    database="lore",
-    user="lore",  # need to setup environment variable
-    password="1234")  # need to setup environment variable
+    database="bankapp",
+    user="Daniel",  # need to setup environment variable
+    password="placeholder")  # need to setup environment variable
 
 # Open a cursor to perform database operations
 db_cursor = db_connection.cursor()
